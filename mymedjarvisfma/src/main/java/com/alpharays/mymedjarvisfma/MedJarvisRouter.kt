cd10e7b2ad90e88ff86b26d33a91676b2068caec
SumitRaaho <sumitkumar@raaho.in>
2024-06-03 23:27:18 +0530

    integ

--- mymedjarvisfma/src/main/java/com/alpharays/mymedjarvisfma/MedJarvisRouter.kt
+++ mymedjarvisfma/src/main/java/com/alpharays/mymedjarvisfma/MedJarvisRouter.kt
@@ -2,10 +2,12 @@ package com.alpharays.mymedjarvisfma
 
 import android.content.Context
 import android.content.Intent
+import androidx.core.content.ContextCompat.startActivity
 
 
 object MedJarvisRouter {
     fun startMedJarvisFma(context: Context) {
         val intent = Intent(context, MedJarvisActivity::class.java)
+        startActivity(context, intent, null)
     }
 }
