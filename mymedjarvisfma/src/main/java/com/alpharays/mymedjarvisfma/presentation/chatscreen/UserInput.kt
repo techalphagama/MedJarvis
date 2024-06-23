cc48fc869d0e19f79331616c210ef1c0bf23f0b8
SumitRaaho <sumitkumar@raaho.in>
2024-06-23 10:24:30 +0530

    waste removed

--- mymedjarvisfma/src/main/java/com/alpharays/mymedjarvisfma/presentation/chatscreen/UserInput.kt
+++ mymedjarvisfma/src/main/java/com/alpharays/mymedjarvisfma/presentation/chatscreen/UserInput.kt
@@ -1,14 +1,12 @@
 package com.alpharays.mymedjarvisfma.presentation.chatscreen
 
 import android.net.Uri
-import androidx.activity.compose.BackHandler
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -41,24 +39,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.focus.focusRequester
-import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.semantics.SemanticsPropertyKey
-import androidx.compose.ui.semantics.SemanticsPropertyReceiver
-import androidx.compose.ui.semantics.contentDescription
-import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
@@ -67,11 +54,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.alpharays.mymedjarvisfma.data.UriCustomSaver
-
-enum class InputSelector {
-    NONE,
-    PICTURE
-}
 
 @Preview
 @Composable
@@ -84,7 +66,6 @@ fun UserInputPreview() {
 fun UserInput(
     onMessageSent: (String, MutableList<Uri>) -> Unit,
     modifier: Modifier = Modifier,
-    resetScroll: () -> Unit = {},
 ) {
     val imageUris = rememberSaveable(saver = UriCustomSaver()) {
         mutableStateListOf()
@@ -93,89 +74,43 @@ fun UserInput(
     val pickMediaLauncher =
         rememberLauncherForActivityResult(contract = ActivityResultContracts.PickVisualMedia()) { imageUri ->
             imageUri?.let {
-                imageUris.clear() //added for single image
+                imageUris.clear() // added for single image
                 imageUris.add(it)
             }
         }
-
-    val focusRequester = remember { FocusRequester() }
-    var isFirstTimeFocus by rememberSaveable {
-        mutableStateOf(true)
-    }
-    var currentInputSelector by rememberSaveable { mutableStateOf(InputSelector.NONE) }
-    val dismissKeyboard = { currentInputSelector = InputSelector.NONE }
-    val focusManager = LocalFocusManager.current
-
-    if (currentInputSelector != InputSelector.NONE) {
-        BackHandler(onBack = dismissKeyboard)
-    }
 
     var textState by rememberSaveable(stateSaver = TextFieldValue.Saver) {
         mutableStateOf(TextFieldValue())
     }
 
-    var textFieldFocusState by remember { mutableStateOf(false) }
-
-    var isExpanded by remember {
-        mutableStateOf(false)
-    }
-
-    var isSendEnable by remember {
-        mutableStateOf(false)
-    }
-
     Surface(tonalElevation = 2.dp, contentColor = MaterialTheme.colorScheme.secondary) {
         Column {
-            AnimatedVisibility(visible = !isExpanded) {
-                Row(
-                    modifier = modifier
-                        .fillMaxWidth()
-                        .height(60.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    UserInputSelector(
-                        onClick = {
-                            focusManager.clearFocus()
-                            pickMediaLauncher.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
-                        }
-                    )
-//
-                    UserInputText(
-                        textFieldValue = textState,
-                        onTextChanged = { textState = it },
-                        // Only show the keyboard if there's no input selector and text field has focus
-                        keyboardShown = currentInputSelector == InputSelector.NONE && textFieldFocusState,
-                        // Close extended selector if text field receives focus
-                        onTextFieldFocused = { focused ->
-                            if (focused) {
-                                currentInputSelector = InputSelector.NONE
-                                resetScroll()
-                            }
-                            textFieldFocusState = focused
-                        },
-                        onMessageSent = {
-                            onMessageSent(textState.text, imageUris)
-                            textState = TextFieldValue()
-                        },
-
-                        isFirstTimeFocus = isFirstTimeFocus,
-                        isFirstTimeFocusUpdate = { value ->
-                            isFirstTimeFocus = value
-                        },
-                        modifier = modifier
-                            .weight(1f),
-                        focusRequester = focusRequester,
-                        onSendEnable = { isEnable ->
-                            isSendEnable = isEnable
-                        }
-                    )
-                    SendMessage(
-                        onMessageSent = {
-                            onMessageSent(textState.text, imageUris)
-                            textState = TextFieldValue()
-                        }
-                    )
-                }
+            Row(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .height(60.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                UserInputSelector(
+                    onClick = {
+                        pickMediaLauncher.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+                    }
+                )
+                UserInputText(
+                    textFieldValue = textState,
+                    onTextChanged = { textState = it },
+                    onMessageSent = {
+                        onMessageSent(textState.text, imageUris)
+                        textState = TextFieldValue()
+                    },
+                    modifier = Modifier.weight(1f) // Ensure it takes available space
+                )
+                SendMessage(
+                    onMessageSent = {
+                        onMessageSent(textState.text, imageUris)
+                        textState = TextFieldValue()
+                    }
+                )
             }
             SelectorExpanded(imageUris = imageUris)
         }
@@ -190,29 +125,14 @@ private fun UserInputText(
     onTextChanged: (TextFieldValue) -> Unit,
     textFieldValue: TextFieldValue,
     onMessageSent: (String) -> Unit,
-    onSendEnable: (Boolean) -> Unit,
-    keyboardShown: Boolean,
-    onTextFieldFocused: (Boolean) -> Unit,
-    isFirstTimeFocus: Boolean,
-    isFirstTimeFocusUpdate: (Boolean) -> Unit,
-    focusRequester: FocusRequester
 ) {
     val a11ylabel = "textfield_desc"
     Box(modifier = modifier.fillMaxWidth()) {
         UserInputTextField(
             textFieldValue,
             onTextChanged,
-            onTextFieldFocused,
-            onSendEnable,
             onMessageSent,
             keyboardType,
-            isFirstTimeFocus,
-            isFirstTimeFocusUpdate,
-            Modifier.semantics {
-                contentDescription = a11ylabel
-                keyboardShownProperty = keyboardShown
-            },
-            focusRequester = focusRequester
         )
     }
 }
@@ -221,19 +141,12 @@ private fun UserInputText(
 private fun UserInputTextField(
     textFieldValue: TextFieldValue,
     onTextChanged: (TextFieldValue) -> Unit,
-    onTextFieldFocused: (Boolean) -> Unit,
-    onSendEnable: (Boolean) -> Unit,
     onMessageSent: (String) -> Unit,
     keyboardType: KeyboardType,
-    isFirstTimeFocus: Boolean,
-    onFirstTimeFocusUpdate: (Boolean) -> Unit,
     modifier: Modifier = Modifier,
-    focusRequester: FocusRequester
 ) {
-    var lastFocusState by remember { mutableStateOf(false) }
-    var textFieldLoaded by remember { mutableStateOf(false) }
 
-    if (textFieldValue.text.isEmpty() || textFieldValue.text.isBlank()) onSendEnable(false)
+
 
     Row(
         horizontalArrangement = Arrangement.SpaceBetween,
@@ -249,26 +162,9 @@ private fun UserInputTextField(
                 value = textFieldValue,
                 onValueChange = {
                     onTextChanged(it)
-                    onSendEnable(it.text.isNotEmpty() && it.text.isNotBlank())
                 },
                 modifier = modifier
-                    .padding(start = 12.dp, end = 8.dp)
-                    .onFocusChanged { state ->
-                        if (lastFocusState != state.isFocused) {
-                            onTextFieldFocused(state.isFocused)
-                        }
-                        lastFocusState = state.isFocused
-                    }
-                    .focusRequester(focusRequester)
-                    .onGloballyPositioned {
-                        if (isFirstTimeFocus) {
-                            if (!textFieldLoaded) {
-                                focusRequester.requestFocus()
-                                textFieldLoaded = true
-                            }
-                            onFirstTimeFocusUpdate(false)
-                        }
-                    },
+                    .padding(start = 12.dp, end = 8.dp),
                 keyboardOptions = KeyboardOptions(
                     keyboardType = keyboardType,
                     imeAction = ImeAction.Send,
@@ -282,11 +178,10 @@ private fun UserInputTextField(
                 textStyle = TextStyle(color = Color.Black)
             )
             Row(modifier = modifier.fillMaxSize()) {
-                Box(modifier = modifier
-                    .fillMaxSize()
-                    .clickable {
-                        focusRequester.requestFocus()
-                    }
+                Box(
+                    modifier = modifier
+                        .fillMaxSize()
+
                 )
             }
             if (textFieldValue.text.isEmpty()) {
@@ -300,8 +195,6 @@ private fun UserInputTextField(
     }
 }
 
-val KeyboardShownKey = SemanticsPropertyKey<Boolean>("KeyboardShownKey")
-var SemanticsPropertyReceiver.keyboardShownProperty by KeyboardShownKey
 
 @Composable
 fun UserInputSelector(
@@ -348,15 +241,14 @@ fun SendMessage(
     onMessageSent: () -> Unit
 ) {
     IconButton(
-        onClick = onMessageSent
+        onClick = onMessageSent,
+        modifier = Modifier.size(48.dp) // Adjust size if needed
     ) {
         Icon(
             imageVector = Icons.Filled.Send,
             tint = Color.Blue,
-            modifier = Modifier
-                .padding(8.dp)
-                .size(56.dp),
-            contentDescription = "fast emoji"
+            modifier = Modifier.size(24.dp), // Adjust inner icon size if needed
+            contentDescription = "Send message"
         )
     }
 }
