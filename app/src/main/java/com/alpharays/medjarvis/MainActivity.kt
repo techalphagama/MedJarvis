6f823a0f3047d513b0a9998b0c99c8fdd4aafeb2
sumitraaho <sumitkumar@raaho.in>
2024-12-10 00:05:01 +0530

    wrapper added

---
+++ app/src/main/java/com/alpharays/medjarvis/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.alpharays.medjarvis
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import com.alpharays.medjarvis.ui.theme.MedJarvisTheme
+import com.alpharays.mymedjarvisfma.MedJarvisRouter
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MedJarvisTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    Text(
+        text = "Hello $name!",
+        modifier = modifier.clickable {
+            MedJarvisRouter.startMedJarvisFma(context = context)
+        }
+    )
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MedJarvisTheme {
+        Greeting("Android")
+    }
+}
