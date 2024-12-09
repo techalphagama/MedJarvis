156f7958afa8bcdc999766f846ddafeece06c8c8
sumitraaho <sumitkumar@raaho.in>
2024-12-10 00:06:21 +0530

    git ignore

---
+++ app/src/main/java/com/alpharays/medjarvis/MedJarvisApp.kt
@@ -0,0 +1,14 @@
+package com.alpharays.medjarvis
+
+import android.app.Application
+import com.alpharays.mymedjarvisfma.MedJarvisRouter
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MedJarvisApp : Application() {
+    //set context
+    override fun onCreate() {
+        super.onCreate()
+        MedJarvisRouter.context = this
+    }
+}
